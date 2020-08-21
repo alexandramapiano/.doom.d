f2346bcb2be4e07f1484b7ef1f77fb52d673b761
Earnest Ma <earnestma@protonmail.com>
2020-08-21 12:56:09 -0400

    Add basic org config

--- config.el
+++ config.el
@@ -3,11 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
-
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
-(setq user-full-name "Earnest Ma")
-;;      user-mail-address "earnestma@protonmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom. Here
 ;; are the three important ones:
@@ -22,24 +19,26 @@
 ;; (setq doom-font (font-spec :family "monospace" :size 12 :weight 'semi-light)
 ;;       doom-variable-pitch-font (font-spec :family "sans" :size 13))
 
-;; Font - Jetbrains Mono
+
+;; General Configuration --------------------------------------------
+
 (setq
+ user-full-name "Earnest Ma"
  doom-font(font-spec :family "Jetbrains Mono" :size 18)
- doom-big-font(font-spec :family "Jetbrains Mono" :size 33))
+ doom-big-font(font-spec :family "Jetbrains Mono" :size 33)
+ doom-theme 'doom-one
+ display-line-numbers-type t)
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
 ;;(setq org-directory "~/org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
-;; numbers are disabled. For relative line numbers, set this to `relative'.
-(setq display-line-numbers-type t)
-
+;; numbers are disabled. For elative line numbers, set this to `relative'.
 
 ;; Here are some additional functions/macros that could help you configure Doom:
 ;;
@@ -58,17 +57,95 @@
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
 
-;; Agenda Config
-(setq
- org-directory "~/org/")
 
+;; ----------------- Org-Mode Configuration -----------------------------
+;;
+;;
+;;              |\      _,,,---,,_
+;;        ZZZzz /,``'-'`'    -.  ;-;;,_
+;;             |,4-  ) )-,_. ,\ (  `'-'
+;;            '---''(_/--'  `-'\_)
+;;
+;; ----------------------------------------------------------------------
 
-(use-package org-journal
+;; org config goes here
+(use-package! org
   :defer t
+  :ensure org-plus-contrib
+  :init
+  (setq org-todo-keywords '((sequence "TODO(t)" "NEXT(n!)" "PROJ(p!)" "|" "DONE(x!)"
+                                 )
+                       (sequence "WAITING(w@/!)" "SMDAY(s@/!)" "|" "CANCELLED(c@/!)" "PHONE(P)" "MEETING(M)"
+                                 )
+                       ))
+  :custom
+  (calendar-latitude 43.7682) ;;Toronto, ON, Canada
+  (calendar-longitude -79.4126)
+  (org-treat-S-cursor-todo-selection-as-state-change nil)
+  (org-global-properties (quote (("Effort_ALL" . "0:05 0:10 0:15 0:20 0:30 0:45 1:00 1:30 2:00 4:00 6:00 8:00"))
+                                ))
+
+  (org-tag-alist (quote ((:startgroup)
+                         ;; LOCATIONS
+                         ("@errands" . ?E)
+                         ("@office" . ?O)
+                         ("@home" . ?H)
+                         (:endgroup)
+                         ;; DEVICES
+                         (:startgroup)
+                         ("computer" . ?c)
+                         ("server" . ?s)
+                         (:endgroup)
+                         ;; CONTEXTS
+                         ("work" . ?w)
+                         ("email" . ?e)
+                         ("learning" . ?l)
+                         ("self" . ?s)
+                         ("home" . ?h)
+                         )
+                        ))
+  )
+
+;; org-capture config
+(use-package! org-capture
+  :after org
+  :bind (("C-c c" . org-capture))
+  )
+
+;; org-agenda config
+(use-package! org-agenda
+  :after org
+  :bind (("C-c a" . org-agenda))
+  :init
+  (setq org-directory "~/org/")
+  :custom
+  (org-agenda-files '("~/org/"))
+  (org-agenda-dim-blocked-tasks t)
+  (org-enforce-todo-dependencies t)
+  (org-agenda-span 1)
+  )
+
+;; org-journal config
+(use-package! org-journal
+  :after org
+  :diminish org-journal-mode
   :bind (("C-c t" . org-journal-new-entry))
   :init
   (setq org-journal-prefix-key "C-c j")
   :custom
-  (org-journal-dir "~/org/logboek/"
-   org-journal-file-type "weekly"
-   org-journal-file-format "%Y-%m-%d.org"))
+  (org-journal-dir "~/org/logboek/")
+  (org-journal-file-type 'weekly)
+  (org-journal-file-format "%Y-%m-%d.org")
+  )
+
+;; org-crypt config
+;; This is my current org-crypt config
+;; Peeking in .gitignore will tell you that it's called org-crypt-config.el
+;; (use-package org-crypt
+;; :after org
+;; :init
+;; (org-crypt-use-before-save-magic)
+;; :custom
+;; (org-crypt-key "AAAAAAAA...")
+;; )
+(load! "org-crypt-config.el")
